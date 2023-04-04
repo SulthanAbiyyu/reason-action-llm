4794e411bd77e8d3010b66d6b13d398e2bc3c44f
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-04-04 21:57:47 +0700

    feat(llm.py): add LLM class with run method to generate response from OpenAI's GPT-3 model feat(__init__.py): add LLM class to __all__ list for easier import

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from llm import *
+
+__all__ = [
+    'LLM',
+]
+
---
+++ src/llm.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+import os
+import openai
+
+
+class BaseLLM(BaseModel):
+    model: str = "gpt-3.5-turbo"
+    temperature: float = 0.5
+    max_tokens: int = 100
+    top_p: float = 1.0
+    prompt: Optional[str] = None
+    response: Optional[dict] = None
+
+    load_dotenv(".env")
+    openai.api_key = os.getenv("OPENAI_KEY")
+
+    def get_used_tokens(self):
+        return self.response["usage"]["total_tokens"]
+
+    def get_answer(self):
+        return self.response["choices"][0]["message"]["content"]
+
+
+class LLM(BaseLLM):
+    def run(self, prompt: str):
+        self.prompt = prompt
+        self.response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": self.prompt,
+                }
+            ],
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            top_p=self.top_p,
+        )
